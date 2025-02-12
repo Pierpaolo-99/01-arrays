8e943b06892a06926a956b59953c5258dfec46ca
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-12 14:07:06 +0100

    complete ex 3 and 4

--- script.js
+++ script.js
@@ -17,10 +17,16 @@ console.log(fourthTeacher);
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers[6];
+console.log(lastTeacher);
+teachers.pop()
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+teachers.shift()
+console.log(firstTeacher);
+
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
